4577b99827561e1580643ae350f421d0e9c99374
Jesus Lopez <101004975+j1zuzz@users.noreply.github.com>
2025-06-02 21:53:17 +0000

    He actualizado las dependencias de Jest y ts-jest a las versiones 29 para asegurar compatibilidad con TypeScript 5.x

---
+++ lib/Utils/decode-wa-message.d.ts
@@ -0,0 +1,38 @@
+import { proto } from '../../WAProto';
+import { ILogger } from './logger';
+import { SignalRepository, WAMessageKey } from '../Types';
+import { BinaryNode } from '../WABinary';
+export declare const NO_MESSAGE_FOUND_ERROR_TEXT = "Message absent from node";
+export declare const MISSING_KEYS_ERROR_TEXT = "Key used already or never filled";
+export declare const NACK_REASONS: {
+    ParsingError: number;
+    UnrecognizedStanza: number;
+    UnrecognizedStanzaClass: number;
+    UnrecognizedStanzaType: number;
+    InvalidProtobuf: number;
+    InvalidHostedCompanionStanza: number;
+    MissingMessageSecret: number;
+    SignalErrorOldCounter: number;
+    MessageDeletedOnPeer: number;
+    UnhandledError: number;
+    UnsupportedAdminRevoke: number;
+    UnsupportedLIDGroup: number;
+    DBOperationFailed: number;
+};
+type GetMessage = (key: WAMessageKey) => Promise<proto.IMessage | undefined>;
+/**
+ * Decode the received node as a message.
+ * @note this will only parse the message, not decrypt it
+ */
+export declare function decodeMessageNode(stanza: BinaryNode, meId: string, meLid: string): {
+    fullMessage: proto.IWebMessageInfo;
+    author: string;
+    sender: string;
+};
+export declare const decryptMessageNode: (stanza: BinaryNode, meId: string, meLid: string, repository: SignalRepository, logger: ILogger, getMessage: GetMessage) => {
+    fullMessage: proto.IWebMessageInfo;
+    category: string;
+    author: string;
+    decrypt(): Promise<void>;
+};
+export {};
